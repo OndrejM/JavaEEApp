0074434c6625e6184f132c39ed5a4644e26b82e8
ondro <ondro@ondro-Lenovo-Flex-2-15>
2015-06-26 10:17:04 +0200

    Scope

--- src/main/java/sk/foxconn/caradmin/view/CarView.java
+++ src/main/java/sk/foxconn/caradmin/view/CarView.java
@@ -1,12 +1,14 @@
 package sk.foxconn.caradmin.view;
 
 import java.util.List;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import sk.foxconn.caradmin.logic.CarAdmin;
 import sk.foxconn.caradmin.model.Car;
 
 @Named("carController")
+@RequestScoped
 public class CarView extends BaseView {
  
  @Inject
@@ -15,4 +17,5 @@ public class CarView extends BaseView {
  public List<Car> getCars() {
   return carAdmin.getAllCars();
  }
+ 
 }
